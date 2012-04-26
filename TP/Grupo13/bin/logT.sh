e043eb587d09fd498b58fa05cc6ad902b3df0b48
arielik <arielik@gmail.com>
2012-04-26 14:18:28 +0000

    Log arreglado y prueba de llamada con variables de entorno definidas

--- TP/Grupo13/bin/logT.sh
+++ TP/Grupo13/bin/logT.sh
@@ -15,22 +15,22 @@ USAGE="USAGE: logT command [message_type] message\
 truncate() {
 
     SIZE=0
-    #Obtengo el tamaño del archivo, si es que este existe.
+    #Obtengo el tamani del archivo, si es que este existe.
     if [ -e "$LOGDIR/$COMMAND.$LOGEXT" ]
     then
           SIZE=`du -sk "$LOGDIR/$COMMAND.$LOGEXT"`
           SIZE=`echo $SIZE | cut -f 1 -d ' '`
     fi
 
-        AUXMAX=`expr $LOGSIZE \* 1024`
-    #Si es mas grande que el tamaño maximo permitido, entonces lo trunco.
+    AUXMAX=`expr $LOGSIZE \* 1024`
+    
+    #Si es mas grande que el tamani maximo permitido, entonces lo trunco.
     if [ $SIZE -gt $AUXMAX ]
     then
-                  TOTAL_LINES=`wc -l $LOGDIR/$COMMAND.$LOGEXT | cut -f 1 -d ' '`
+          TOTAL_LINES=`wc -l $LOGDIR/$COMMAND.$LOGEXT | cut -f 1 -d ' '`
           CUTLINES=`expr $TOTAL_LINES \/ 2`
-                  echo -e "`tail -n $CUTLINES "$LOGDIR/$COMMAND.$LOGEXT"`" > "$LOGDIR/$COMMAND.$LOGEXT"
-                  echo "$DATE-$USER-I-LOG EXCEEDED." >> "$LOGDIR/$COMMAND.$LOGEXT"
-
+          echo -e "`tail -n $CUTLINES "$LOGDIR/$COMMAND.$LOGEXT"`" > "$LOGDIR/$COMMAND.$LOGEXT"
+          echo "$DATE-$USER-I-LOG EXCEEDED." >> "$LOGDIR/$COMMAND.$LOGEXT"
     fi
 
     return 0
@@ -38,17 +38,17 @@ truncate() {
 
 #Funcion principal.
 #Parametros
-#     Parámetro 1 (obligatorio): COMMAND
-#     Parámetro 2 (obligatorio): Tipo de MSG
-#     Parámetro 3 (obligatorio): MSG
+#     Parametro 1 (obligatorio): COMMAND
+#     Parametro 2 (obligatorio): Tipo de MSG
+#     Parametro 3 (obligatorio): MSG
 main() {
 
     COMMAND=$1
     MSGTYPE=$2
     DATE=`date "+%Y%m%d_%H:%M:%S"`
-        USER = echo `whoami`
-
-        #Si MSG tiene mas de 140 caracteres lo trunco
+    USER=`whoami`
+    
+    #Si MSG tiene mas de 140 caracteres lo trunco
     MSG=`echo $3 | sed 's/\(^.\{140\}\).*/\1/'`
 
     case $COMMAND in
@@ -65,9 +65,9 @@ main() {
 }
 
 # Chequea que la variable logdir sea un directorio valido (salvo en el caso de la instalacion)
-if [ ! -d $LOGDIR ] && [ ! $COMMAND == "instalar" ]; then
-  echo "No existe el directorio [$LOGDIR]"
-  exit 1
+if [ ! -d "$LOGDIR" ] && [ ! $COMMAND == "instalar" ]  ; then
+   echo "No existe el directorio [$LOGDIR]"
+   exit 1
 fi
 
 #Tiene que tener tres parametros obligatoriamente.
@@ -76,6 +76,6 @@ if [ $# \< 3 ] || [ $# \> 3 ]; then
     exit 2
 fi
 
-main $1 $2 $3
+main "$1" "$2" "$3"
 exit 0
 
