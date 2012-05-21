24b35703171a0568b748308b1946dcaafbfb2878
arielik <arielik@5c016c39-f17d-4b71-373e-4c5fb63b864b>
2012-05-21 01:40:56 +0000

    StartD terminado, StopD al 50%, lo sigo manian es rapido

---
+++ trunk/TP/Grupo13/bin/stopD.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Grupo: 13
+# Name: iniciarT.sh
+
+source global.sh
+
+COMANDO="stopD"
+
+chequeaProceso(){
+
+  #El Parametro 1 es el proceso que voy a buscar
+  PROC=$1
+  PROC_LLAMADOR=$2
+
+  #Busco en los procesos en ejecucion y omito "grep" ya que sino siempre se va a encontrar a si mismo
+  # -w es para que busque coincidencia exacta en la palabra porque sino estamos obteniendo cualquier cosa.
+  PID=`ps ax | grep -v $$ | grep -v grep | grep -v -w "$PROC_LLAMADOR" | grep $PROC`
+  PID=`echo $PID | cut -f 1 -d ' '`
+  echo $PID
+  
+}
+
