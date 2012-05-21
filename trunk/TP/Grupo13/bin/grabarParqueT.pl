dab8cd2144dc55e6b9c4f9c34c7185667b99bc3f
arielcorreofiuba <arielcorreofiuba@5c016c39-f17d-4b71-373e-4c5fb63b864b>
2012-05-21 12:54:17 +0000

    otra actualizacion de grabarParqueT

--- trunk/TP/Grupo13/bin/grabarParqueT.pl
+++ trunk/TP/Grupo13/bin/grabarParqueT.pl
@@ -17,15 +17,16 @@
 
 ################################################################################
 #defino aca las constantes que estan en instalarT.conf (hago asi por cuestiones de desarrollo rapido)...
-$grupo                  = "/home/ariel/ssoo1c-2012/TP/Grupo13";
-$ejecutable_loguearT	= $grupo."/bin/loguearT.sh";
-$nombre_grabarParqueT 	= "GrabarParqueT.pl";
-$dir_inst_recibidas 	= $grupo."/inst_recibidas";
-$dir_inst_rechazadas 	= $grupo."/inst_rechazadas";
-$dir_inst_procesadas 	= $grupo."/inst_procesadas";
-$dir_inst_ordenadas 	= $grupo."/inst_ordenadas";
+$GLOBAL_grupo                  	= "/home/mart/ssoo1c-2012/TP/Grupo13";
+$GLOBAL_ejecutable_loguearT		= $GLOBAL_grupo."/bin/loguearT.sh";
+$GLOBAL_ejecutable_moverT		= $GLOBAL_grupo."/bin/moverT.pl";
+$GLOBAL_nombre_grabarParqueT 	= "GrabarParqueT.pl";
+$GLOBAL_dir_inst_recibidas 		= $GLOBAL_grupo."/inst_recibidas";
+$GLOBAL_dir_inst_rechazadas 	= $GLOBAL_grupo."/inst_rechazadas";
+$GLOBAL_dir_inst_procesadas 	= $GLOBAL_grupo."/inst_procesadas";
+$GLOBAL_dir_inst_ordenadas 		= $GLOBAL_grupo."/inst_ordenadas";
 
-if ( !(-d $grupo) ){
+if ( !(-d $GLOBAL_grupo) ){
     print "ERROR SEVERO: corregir la variable \$grupo en grabarParqueT.pl\n";
     exit 2;
 }
@@ -76,26 +77,74 @@ sub inicializarLogCon{
 		$cant_archivosProcesar_ = @_[0];
 		
 		#le doy permiso de ejecucion al comando LoguearT si es que no lo tiene
-        if (!(-x $ejecutable_loguearT)){
-            print $ejecutable_loguearT.": NO es ejecutable\n";
-            $darPermiso = `chmod +x $ejecutable_loguearT`;
+        if (!(-x $GLOBAL_ejecutable_loguearT)){
+            print $GLOBAL_ejecutable_loguearT.": NO es ejecutable\n";
+            $darPermiso = `chmod +x $GLOBAL_ejecutable_loguearT`;
         }		
         
-		$grabarLog = `$ejecutable_loguearT $nombre_grabarParqueT I \"Inicio de GrabarparqueT: \<$cant_archivosProcesar_\>\"`;
+		$grabarLog = `$GLOBAL_ejecutable_loguearT $GLOBAL_nombre_grabarParqueT I \"Inicio de GrabarparqueT: \<$cant_archivosProcesar_\>\"`;
 		#print "Log: ";
 		#print $grabarLog;
 }
 
 ################################################################################
+#verificar si el archivo pasado por parametro proveniente de inst_ recibidas ya existe en inst_procesadas
+sub esArchivoDuplicado_En{
+	$archivo_comprobar = @_[0];
+	$dir_inst_recibidas_ = $GLOBAL_dir_inst_recibidas;
+	$esArchivoDuplicado = "si";
+	
+	
+	return ( $esArchivoDuplicado );
+}
+
+################################################################################
+sub exite_archivo_En{
+		$arch_origen = @_[0];
+		$dir_destino = @_[1];
+		
+		
+		$exite_archivo_origen_ = "no";
+		
+		if (opendir(DIRH,"$dir_destino"))
+		{
+			@flist=readdir(DIRH);
+			closedir(DIRH);
+		}
+		foreach (@flist){
+			# ignorar . y .. :
+			next if ($_ eq "." || $_ eq "..");
+			if ( -r "$dir_destino/$_" ){
+				if ( $_ eq $arch_origen ){
+					$exite_archivo_origen_ = "si";
+				}
+			}
+		}
+		closedir(DIRH);
+
+		return ($exite_archivo_origen_);
+}
+
+################################################################################
 sub procesarArchivoRecibido{
 	$nombre_archivo_recibido = @_[0];
-	$grabarLog = `$ejecutable_loguearT $nombre_grabarParqueT I \"Archivo a Procesar: \<$nombre_archivo_recibido\>\"`;
-    #print "Log: ";
-    #print $grabarLog;
-	#TODO: proceso del archivo recibido... 
+	$grabarLog = `$GLOBAL_ejecutable_loguearT $GLOBAL_nombre_grabarParqueT I \"Archivo a Procesar: \<$nombre_archivo_recibido\>\"`;
+	$esArchivoDuplicado = "si";
+	
     $archivo_procesar = $dir_archivosProcesar_."/".$nombre_archivo_recibido;
-    print $archivo_procesar."\n";
-
+    
+    #se considera los archivos duplicados
+    $esArchivoDuplicado = exite_archivo_En($nombre_archivo_recibido, $GLOBAL_dir_inst_procesadas);
+    if ( $esArchivoDuplicado eq "si" )
+    {
+    	$grabarLog = `$GLOBAL_ejecutable_loguearT $GLOBAL_nombre_grabarParqueT I \"En inst_procesadas ya existe el archivo: $nombre_archivo_recibido\.Se lo mueve hasta inst_rechazadas\"`;
+    	
+    	$moverDuplicado = `$GLOBAL_ejecutable_moverT $archivo_procesar $GLOBAL_dir_inst_rechazadas`;
+    	
+    }else{
+		#TODO: continuar con los no duplicados
+		print $archivo_procesar."\n";
+    }
 }
 
 ################################################################################
@@ -107,7 +156,7 @@ sub principal{
 	#verifica si se puede iniciar la ejecucion del comando...
 	if ( $esAmbienteInit && $corriendoGrabarParque )
 	{
-		@nombres_ArhivosProcesar = getNombresArchivosProcesar($dir_inst_recibidas);
+		@nombres_ArhivosProcesar = getNombresArchivosProcesar($GLOBAL_dir_inst_recibidas);
 		$cant_archivosProcesar = @nombres_ArhivosProcesar;
 		inicializarLogCon($cant_archivosProcesar);
 		
