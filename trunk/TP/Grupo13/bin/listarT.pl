f353f51cd156d11b7c337560fad8ea766b8d225a
martineq@gmail.com <martineq@gmail.com@5c016c39-f17d-4b71-373e-4c5fb63b864b>
2012-05-23 22:50:44 +0000

    Actualizo al momento de la entrega (no están los cambios hechos en el momento de la entrega).-

--- trunk/TP/Grupo13/bin/listarT.pl
+++ trunk/TP/Grupo13/bin/listarT.pl
@@ -1,161 +1,262 @@
 #!/usr/bin/perl 
 
-#########################################
-#					#
-#	Sistemas Operativos 75.08	#
-#	Grupo: 	13			#
-#	Nombre:	listarT.pl		#
-#					#
-#########################################
-
-################################################################################
-# 1. El propósito de este programa perl es resolver cualquier tipo de consulta efectuada sobre el
-# parque instalado y emitir los informes correspondientes cuando se lo requiera.
-# 2. Se alimenta de los archivos de parque instalado y de los archivos maestros.
-# 3. Debe aceptar al menos los parámetros tipo de producto, ID de sucursal, Id de Cliente y la
-# Descripción del Ítem de Producto del Registro Cabecera
-# 4. Debe aceptar las opciones –c –e –h la combinación de las dos primeras.
-# 4.1. –c resuelve la consulta y muestra resultados no graba en archivo
-# 4.2. –e resuelve la consulta y graba un reporte
-# 4.3. –h muestra la ayuda del comando
-# 5. Si lo desean pueden agregar más parámetros y más opciones
-# 6. El nombre del archivo que se graba al emitir el informe debe ser lpi.<SEC> donde SEC es
-# un número de secuencia queda al criterio del desarrollador su definición, pero siempre
-# debe ser un nombre nuevo, no debe sobrescribir ningún informe previo.
-# 7. Los informes se graban en el directorio: REPODIR
-# 8. En el encabezado siempre mostrar titulo del reporte y los parámetros de invocación
-# 9. En los subtítulos, las etiquetas de los campos
-# 10. Mostrar los siguientes campos:
-# 10.1. Sucursal, además del Id, mostrar el BRANCH_NAME
-# 10.2. Cliente, además del Id, mostrar el CUSTOMER_1ST_NAME
-# 10.3. Tipo de producto (PRODUCT_TYPE_NAME)
-# 10.4. Plan Comercial, corresponde a la Desc. del Ítem de Prod. del Reg. Cabecera
-# 10.5. Item, corresponde a la Descripción del Ítem de Prod. del Reg. de detalle
-# 11. La salida siempre debe estar ordenada (criterio a elección) y mostrar subtotales y totales
-# (también a elección sobre que campos se calculan los totales)
-################################################################################
-
 
 ################################################################################
 #defino aca las constantes que estan en instalarT.conf (hago asi por cuestiones de desarrollo rapido)...
-$GLOBAL_grupo                  	= "/home/mart/ssoo1c-2012/TP/Grupo13";
 
-$dir_productos = $GLOBAL_grupo."/maestro/prod.mae";
-$dir_sucursales = $GLOBAL_grupo."/maestro/sucu.mae";
-$dir_clientes = $GLOBAL_grupo."/maestro/cli.mae";
 
+@bufferOutput =();
+%prodHash;
+%cliHash;
+%sucuHash;
+	
 sub parseConfig{
 
+$grupo = "/var/tmp/TP/Grupo13";
+$maeDir = "/var/tmp/TP/Grupo13/mae";
+$cliMae = $maeDir."/cli.mae";
+$prodMae = $maeDir."/prod.mae";
+$sucMae = $maeDir."/sucu.mae";
+$parqueInstalado=$grupo."/parque_instalado";
+$reportes=$grupo."/reportes";
+#/var/tmp/TP/Grupo13/parque_instalado/TVPORCABLE
 }
 
 sub loadHashes{
-
-	$dir_pro_ = $dir_productos;
-	$dir_suc_ = $dir_sucursales;
-	$dir_cli_ = $dir_clientes;
-	
-	@array_pro;
-	@array_suc;
-	@array_cli;
-			
-	$error_open_pro = "si";
-	$error_open_suc = "si";
-	$error_open_cli = "si";
-	
 # Abre los archivos de productos, sucursales y clientes
-	if ( open(FILE_PRO," < $dir_pro_") ){
-		$error_open_pro = "no";
-		@array_pro = <FILE_PRO>;
-		close(FILE_PRO);
-	}
-	if ( open(FILE_SUC," < $dir_suc_") ){
-		$error_open_suc = "no";
-		@array_suc = <FILE_SUC>;
-		close(FILE_SUC);
-	}
-	if ( open(FILE_CLI," < $dir_cli_") ){
-		$error_open_cli = "no";
-		@array_cli = <FILE_CLI>;
-		close(FILE_CLI);
-	}		
-# En caso de error informa y sale
-	if ($error_open_pro eq "si" || $error_open_suc eq "si" || $error_open_cli eq "si"){
-		if ($error_open_pro eq "si"){ print "error lectura de productos\n";}
-		if ($error_open_suc eq "si"){ print "error lectura sucursales\n";}
-		if ($error_open_cli eq "si"){ print "error lectura clientes\n";}				
-		exit 1;
-	}
+open F_CLIENTES, "<", "$cliMae" or die "No se pudo abrir el archivo de $cliMae";
+open F_PRODUCTOS, "<", "$prodMae" or die "No se pudo abrir el archivo de $cliMae";
+open F_SUCURSALES, "<", "$sucMae" or die "No se pudo abrir el archivo de $cliMae";
 
 # Recorre secuencialmente el archivo
-
-# Realiza split dejando los campos que interesan para el hash, clave y valor
-
-# Carga el hash de productos, valor clave: itemName, valor asoc.: idTypeName
-
-
-# Recorre secuencialmente el archivo
-
-# Realiza split dejando los campos que interesan para el hash, clave y valor
-
-# Carga el hash de sucursales, valor clave: idSuc, valor asoc.: branchName
-
-
-# Recorre secuencialmente el archivo
-
-# Realiza split dejando los campos que interesan para el hash, clave y valor
-
-# Carga el hash de clientes, valor clave: idClient, valor asoc.: clientName
-
-exit 0;
-
+while (<F_CLIENTES>){
+	chomp;
+	# Realiza split dejando los campos que interesan para el hash, clave y valor
+	($idCliente, $territori, $cliName,$resto)=split(";");
+	# Carga el hash de clientes, valor clave: idCliente, valor asoc.: clientName
+	$cliHash{$idCliente}=$cliName;
 }
+
+# Recorre secuencialmente el archivo
+while (<F_SUCURSALES>){
+	chomp;
+	# Realiza split dejando los campos que interesan para el hash, clave y valor
+	($idRegion, $regionName, $idSuc,$sucName,$resto)=split(",");
+	# Carga el hash de sucursales, valor clave: idSuc, valor asoc.: sucName
+	$sucHash{$idSuc}=$sucName;
+}
+
+
+# Recorre secuencialmente el archivo
+while (<F_PRODUCTOS>){
+	chomp;
+	# Realiza split dejando los campos que interesan para el hash, clave y valor
+	($prodTypeId, $prodTypeName, $a, $s, $d, $f, $g, $h, $itemName)=split(",");
+	# Carga el hash de productos, valor clave: prodTypeName, valor asoc.: itemName
+	$sucHash{$prodTypeName}=$itemName;
+}
+
+# Cierro archivos
+
+close (F_CLIENTES);
+close (F_SUCURSALES);
+close (F_PRODUCTOS);
+
+};
 
 sub parseArgs{
 
-# Si encuentra -h imprime ayuda y sale
+	$cantParams = @ARGV;
+	$matchSucFlag=0;
+	$matchCliFlag=0;
+	$matchStrFlag=0;
+	$printFlag=0;
+	$printScreen=0;
+	
+	if($cantParams == 1) {
+	# Si encuentra -h imprime ayuda y sale
+        if($ARGV[0] eq '-h' || $ARGV[0] eq '--help') {
+            printHelp();
+	    exit 1;
+        }
+        else {
+            print "Comando incorrecto \n";
+		printHelp();
+		exit 1;
+        }
+        return 1;
+    }
+	
+	for($i = 0; $i < $cantParams; $i++) {
+	
+		if ( $ARGV[$i] eq '-c' ) {
+			# Si encuentra -c setea el flag de impresion solo por pantalla
+			$printScreen=1;
+		}
+	
+		if ( $ARGV[$i] eq '-e' ) {
+			# Si encuentra -e setea el flag de impresion en archivo
+			$printFlag=1;	
+		}
+		
+		if ( $ARGV[$i] eq '-t' ) {
+			# Si encuentra -t determina un arreglo con los archivos a mirar (nombre). Tambien valida que sean validos y
+			# que si es * exista un archivo en el directorio
+			$i++;
+			while ( (($ARGV[$i] ne '-e') &&($ARGV[$i] ne '-c') &&($ARGV[$i] ne '-h') &&($ARGV[$i] ne '-s') &&($ARGV[$i] ne '-k') &&($ARGV[$i] ne '-p')) && ($i < $cantParams) ){
+				print "$i ,  $ARGV[$i]";
+				@filesToProcess=$ARGV[$i];
+				$i++;
+			}
+		}
+		
+		if ( $ARGV[$i] eq '-s' ) {
+		# Si encuentra -s guarda un array de id sucursales a matchear, si es * lo guarda y es interpretado luego como any
+		# Tambien realiza la validacion de los elementos, si alguno no es numerico (a excepcion de *) devuelve error.
+			$i++;
+			$j=0;
+			while ( (($ARGV[$i] ne '-e') &&($ARGV[$i] ne '-c') &&($ARGV[$i] ne '-h') &&($ARGV[$i] ne '-s') &&($ARGV[$i] ne '-k') &&($ARGV[$i] ne '-p')) && ($i < $cantParams) ){
+				@sucArray=$ARGV[$i];
+				$matchSucFlag=1;
+				$i++;
+				$j++;
+				if ($j > 1){
+					print "El rango no pueden ser mas de dos elementos!";
+					exit 1;
+				}
+			}
+		}
 
-# Si encuentra -c setea el flag de impresion solo por pantalla
-
-# Si encuentra -e setea el flag de impresion en archivo
-
-# Si encuentra -t determina un arreglo con los archivos a mirar (nombre). Tambien valida que sean validos y
-# que si es * exista un archivo en el directorio
-
-# Si encuentra -s guarda un array de id sucursales a matchear, si es * lo guarda y es interpretado luego como any
-# Tambien realiza la validacion de los elementos, si alguno no es numerico (a excepcion de *) devuelve error.
-
-# Si encuentra -k guarda un array de id clientes a matchear, si es * lo guarda y es interpretado luego como any
-# Tambien realiza la validacion de los elementos, si alguno no es numerico (a excepcion de *) devuelve error.
-
-
-# Si encuentra -p guarda el string a matchear con el campo itemName. 
-
-#Si hay mas de 11 tira error por exceder el maximo
+		if ( $ARGV[$i] eq '-k' ) {
+		# Si encuentra -k guarda un array de id clientes a matchear, si es * lo guarda y es interpretado luego como any
+		# Tambien realiza la validacion de los elementos, si alguno no es numerico (a excepcion de *) devuelve error.
+			$i++;
+			while ( (($ARGV[$i] ne '-e') &&($ARGV[$i] ne '-c') &&($ARGV[$i] ne '-h') &&($ARGV[$i] ne '-s') &&($ARGV[$i] ne '-k') &&($ARGV[$i] ne '-p')) && ($i < $cantParams) ){
+				@cliArray=$ARGV[$i];
+				$matchCliFlag=1;
+				$i++;
+			}
+		}
+		
+		if ( $ARGV[$i] eq '-p' ) {
+			# Si encuentra -p guarda el string a matchear con el campo itemName.
+			$i++;
+			while ( (($ARGV[$i] ne '-e') &&($ARGV[$i] ne '-c') &&($ARGV[$i] ne '-h') &&($ARGV[$i] ne '-s') &&($ARGV[$i] ne '-k') &&($ARGV[$i] ne '-p')) && ($i < $cantParams) ){
+				$stringToMatch=$ARGV[$i];
+				$matchStrFlag=1;
+			}
+		}
+	}
 
 }
 
+sub addElementToBuffer(){
+	push(@bufferOutput,[$f_idSuc,$sucHash{$f_idSuc},$f_idCli,$cliHash{$f_idCli},$prodTypeNameComp,$prodHash{$prodTypeNameComp}]);
+}
 
 sub generateOutputData{
 
+
 # Por cada archivo en el array lo abro y recorro secuencialmente
+# Si file to process es * debo leer todos los archivos del directorio
+print $filesToProcess[0];
+if ( $filesToProcess[0] == "*" ){
+	$i=0;
+	opendir(DIR, $parqueInstalado) or die $!;
+	while ( my $file = readdir(DIR)) {
+        # Evito todo lo que comienze con "."
+        if ($file =~ m/^\./){
+		$i++;
+		next;
+		}
+		$filesToProcess[$i] = $file;
+    }
+    closedir(DIR);
+}
 
-# Aplico los filtros de producto, cliente y sucursal
+foreach (@filesToProcess ){
+	open F_INPUT, "<", "$parqueInstalado/$_" or die "No se pudo abrir el archivo de $parqueInstalado/$_";
+	print "procesando $parqueInstalado/$_";
+	while (<F_INPUT>){
+		chomp;
+		($f_idSuc, $f_idCli, $desc)=split(",");
+		$flagEscritura = 0;
+		$prodTypeNameComp = $desc;
+		# Aplico los filtros de producto, cliente y sucursal
+		
+		# Primero chequeo si debeo ver la sucursal, si tiene mas de un elemento es busqueda por rango
+		# Sino busqueda precisa o con wildcard
+		if ( $matchSucFlag == 1) {
+			$sucArraySize = $#sucArray + 1;
+			if ($sucArraySize == 2){
+				if ( $f_idSuc > $sucArray[0] && $f_idSuc < $sucArray[1] ){
+					#Esta dentro del rango
+					$flagEscritura = 1;
+				}
+			}
+			if ( $sucArray[0] eq "*"){
+				$flagEscritura = 1;
+			}
+			if ($sucArray[0] == $f_idSuc ){
+				$flagEscritura = 1;
+			}
+			# Si no matchea sigo con el proximo registro
+			if ($flagEscritura == 0){
+				next;
+			}
+		}
+		$flagEscritura=0;
+		if ( $matchCliFlag == 1 ){
+			$cliArraySize = $#cliArray + 1;
+			if ($cliArray[0] == "*"){
+				$flagEscritura = 1;
+			}
+			if ($cliArray[0] == $f_idCli ){
+				$flagEscritura = 1;
+			}
+			if ( $cliArraySize > 1 ){
+				 foreach (@cliArray) {
+					if ( $f_idCli == $_ ){
+						$flagEscritura = 1;
+					}
+				}
+			}
+			# Si no matchea sigo con el proximo registro
+                        if ($flagEscritura == 0){
+                                next;
+                        }
 
-# Si el resultado es OK guardo en mi buffer de salida
-
-# Cierro el archivo
-
+		}
+		$flagEscritura=0;
+		if ( $matchStrFlag == 1 ){
+			if ( $desc =~ $stringToMatch ){
+				#Si es un substring o incluso el string sigo adelante!
+				$prodTypeNameComp = $desc;
+				$flagEscritura = 1;
+			}		
+                        if ($flagEscritura == 0){
+                                next;
+                        }
+		}
+		
+		# Si el resultado es OK guardo en mi buffer de salida
+		addElementToBuffer();
+	}
+	# Cierro el archivo
+	close (F_INPUT);
+}
 
 # Ordeno buffer de salida por idCliente decreciente
+# sort
 
-}
+};
 
 
 sub printData{
 
 # Imprimo mi buffer por stdout
 
-# Si el flag -e guardo en archivo
+# Si el flag es -e guardo en archivo
 
 # Creo archivo en repositorio default con nombre lpi.<aaaammddhhmmss>
 
@@ -163,7 +264,19 @@ sub printData{
 
 # Cierro el archivo de reporte
 
-
+	if ($printFlag == 1){
+		for $aref ( @bufferOutput ) {
+			print "\t [ @$aref ],\n";
+		};
+	}
+	
+	if ($printScreen == 1){
+		
+		open F_REPORTE, ">", "$reportes/reporte.rep" or die "No se pudo abrir el archivo de $cliMae";
+		for $aref ( @bufferOutput ) {
+			print F_REPORTE "\t [ @$aref ],\n"; 
+		};
+	}
 }
 
 sub printHelp{
@@ -171,30 +284,29 @@ sub printHelp{
 # Imprime informacion de uso de la herramienta
 # Usage:	listarT.pl -<c|e|h|s|k|p|t>
 #	
-#
-#
-#
-#
-#
-#
-#
-#
-
+	print "USAGE: listarT.pl -<c|e|h|s|k|p|t> ";
 
 }
 
 
 # main()
 
+if(parseArgs(@ARGV) != 0){
+		printHelp();
+        exit;
+}
+
 parseConfig();
-parseArgs();
+
 loadHashes();
 generateOutputData();
 printData();
 
 
 
-
+		for $aref ( @bufferOutput ) {
+			print "\t [ @$aref ],\n";
+		};
 
 
 
